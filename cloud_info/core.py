0d2ce1b69e6d47b80217d2047042ba81f1de5a95
Baptiste Grenier <baptiste.grenier@egi.eu>
2016-07-31 09:11:00 +0000

    Revert "core: remove useless arguments"
    This reverts commit 90b056dd3cc623db7ba342010cddf0516db3c326.

--- cloud_info/core.py
+++ cloud_info/core.py
@@ -190,7 +190,7 @@ class CloudBDII(BaseBDII):
 
 def parse_opts():
     parser = argparse.ArgumentParser(
-        description='Cloud Information provider',
+        description='Cloud BDII provider',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         fromfile_prefix_chars='@',
         conflict_handler="resolve",
@@ -214,6 +214,21 @@ def parse_opts():
         '--template-extension',
         default='indigo',
         help=('Extension to use for the templates'))
+
+    parser.add_argument(
+        '--full-bdii-ldif',
+        action='store_true',
+        default=False,
+        help=('Whether to generate a LDIF containing all the '
+              'BDII information, or just this node\'s information\n'
+              'NOTE: it does not generate GlueSchema 1.3 information'))
+
+    parser.add_argument(
+        '--site-in-suffix',
+        action='store_true',
+        default=False,
+        help=('Whether to include the site name in the generated DN\'s'
+              'suffix (Use only for execution as a site-BDII provider)'))
 
     parser.add_argument(
         '--middleware',
