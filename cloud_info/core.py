fceb10d65ca583716002e7b9b3eafd2323bae6de
Baptiste Grenier <baptiste.grenier@egi.eu>
2016-08-17 13:34:47 +0200

    core.py: use all info sources

--- cloud_info/core.py
+++ cloud_info/core.py
@@ -252,14 +252,11 @@ def parse_opts():
 def main():
     opts = parse_opts()
 
-    bdii = IndigoComputeBDII(opts)
-    bdii.load_templates()
-    print(bdii.render().encode('utf-8'))
-    # XXX do not care of legacy stuff
-    # for cls_ in (CloudBDII, ComputeBDII, StorageBDII, IndigoComputeBDII):
-    #     bdii = cls_(opts)
-    #     bdii.load_templates()
-    #     print(bdii.render().encode('utf-8'))
+    # XXX drop need for IndigoComputeBDII
+    for cls_ in (CloudBDII, ComputeBDII, StorageBDII, IndigoComputeBDII):
+        bdii = cls_(opts)
+        bdii.load_templates()
+        print(bdii.render().encode('utf-8'))
 
 if __name__ == '__main__':
     main()
